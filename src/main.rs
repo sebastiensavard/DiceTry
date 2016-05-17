8e69c736c341ff70a6daf74f3db1ff3be0def861
sebastiensavard <ssavard@gmail.com>
2016-05-16 22:38:53 -0400

    working dice roll!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
+fn dice_roll(sides: u8) -> u8 {
+//	return rand::random::<u8>() % 6;
+
+let between = Range::new(0,sides);
+let mut rng = rand::thread_rng();
+return between.ind_sample(&mut rng);
+}
+
+
+
 fn main() {
     println!("Hello Sebastien!");
+    println!("your dice roll is:{}", dice_roll(20));
 }
